416c1dcc9efb6aa7dd9206b24cc1ad279d334102
maheswara11 <maheswara11@gmail.com>
2021-12-14 13:22:10 +0700

    Initial commit

---
+++ app/src/main/java/id/mahes/tugasgambarinternet/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.mahes.tugasgambarinternet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dwisetiyo.gambarinternet.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
